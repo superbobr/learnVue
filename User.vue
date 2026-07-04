f4e660dc5a425fc038401a2fef2c7dfe1959c749
leonidseoshnik <leonseoprofi@gmail.com>
2026-07-04 10:19:37 +0300

    fix Counter.vue, add User.vue

---
+++ User.vue
@@ -0,0 +1,9 @@
+<script setup>
+import {ref} from 'vue'
+const username = ref('')
+</script>
+<template>
+<input type="text" v-model="username" placeholder="Введи свое имя">
+</input>
+<h1 v-show="username">Привет {{username}}!</h1>
+</template>
